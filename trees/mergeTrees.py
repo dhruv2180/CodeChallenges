eaf4403161efd0a32e9ec8c79dc5119ba80129f0
Dhruv Bhardwaj <dhruv2180@gmail.com>
2018-10-27 00:58:32 -0400

    Update mergeTrees.py

--- trees/mergeTrees.py
+++ trees/mergeTrees.py
@@ -12,7 +12,7 @@ class Solution(object):
             return None
         
         t1.val=t1.val+t2.val
-        
+ 
         t1.left=self.mergeTrees(t1.left, t2.left)
         
         t1.right=self.mergeTrees(t1.right, t2.right)
